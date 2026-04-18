8b8ee341cd40283e347558c680f315e36a0e0d39
unknown <zshiva35@gmail.com>
2026-04-18 18:44:50 +0545

    feat: Seller Role access added

--- src/main/java/practice/projects/controller/SellerController.java
+++ src/main/java/practice/projects/controller/SellerController.java
@@ -3,12 +3,14 @@ package practice.projects.controller;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
 import io.micronaut.views.View;
 
 @Controller("/")
 public class SellerController {
     @View("sellerdashboard")
     @Get("/seller")
+    @Secured("SELLER") // Only SELLER role can access
     public HttpResponse<Object> Seller()
     {
         return HttpResponse.ok();
