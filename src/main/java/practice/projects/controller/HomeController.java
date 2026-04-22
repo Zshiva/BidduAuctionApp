c6c768ae4fb5347b03a1ac4e0bf42a10fc980900
unknown <zshiva35@gmail.com>
2026-04-22 13:00:57 +0545

    feat : update httpresponse

--- src/main/java/practice/projects/controller/HomeController.java
+++ src/main/java/practice/projects/controller/HomeController.java
@@ -1,17 +1,58 @@
 package practice.projects.controller;
 
+import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.View;
 
 @Controller("/")
 public class HomeController {
-    @View("home")
     @Get("/")
-    public String Product()
+    @Secured(SecurityRule.IS_ANONYMOUS)
+    public HttpResponse<?> home()
     {
-        return "admin";
-
+        return HttpResponse.ok(new HomeResponse(
+            "Biddu Auction App",
+            "Welcome to Biddu Auction - A modern auction platform",
+            "Please login to continue",
+            new LoginLinks(
+                "/users/login",
+                "/users/register"
+            )
+        ));
     }
 
+    @View("home")
+    @Get("/dashboard")
+    @Secured("BIDDER,SELLER")
+    public String dashboard()
+    {
+        return "admin";
+    }
+
+    static class HomeResponse {
+        public String title;
+        public String description;
+        public String message;
+        public LoginLinks links;
+
+        public HomeResponse(String title, String description, String message, LoginLinks links) {
+            this.title = title;
+            this.description = description;
+            this.message = message;
+            this.links = links;
+        }
+    }
+
+    static class LoginLinks {
+        public String login;
+        public String register;
+
+        public LoginLinks(String login, String register) {
+            this.login = login;
+            this.register = register;
+        }
+    }
 }
