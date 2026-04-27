c00bf598dd4c3c8265e8df3caec18d697492827a
Zshiva <zshiva35@gmail.com>
2026-04-27 09:45:48 +0545

    refactor: persist added products via ProductDbRepository

--- src/main/java/practice/projects/usecase/addproduct/AddProductUseCase.java
+++ src/main/java/practice/projects/usecase/addproduct/AddProductUseCase.java
@@ -6,8 +6,11 @@ import practice.projects.platform.exception.BidduAuctionErrorMessage;
 import practice.projects.platform.exception.BidduAuctionException;
 import practice.projects.platform.usecase.UseCase;
 import practice.projects.repository.ProductEntity;
+import practice.projects.repository.jdbc.ProductDbRepository;
+import practice.projects.repository.mapper.DbMapper;
 import freemarker.template.TemplateException;
 import jakarta.inject.Singleton;
+import jakarta.inject.Inject;
 import jakarta.mail.MessagingException;
 
 import java.io.IOException;
@@ -15,6 +18,13 @@ import java.sql.SQLException;
 import java.util.Optional;
 @Singleton
 public class AddProductUseCase implements UseCase<AddProductUseCaseRequest, AddProductUseCaseResponse> {
+
+     private final ProductDbRepository productDbRepository;
+
+     @Inject
+     public AddProductUseCase(ProductDbRepository productDbRepository) {
+         this.productDbRepository = productDbRepository;
+     }
 
 
     private String findByProductSource(String productSource){
@@ -25,7 +35,7 @@ public class AddProductUseCase implements UseCase<AddProductUseCaseRequest, AddP
     public Optional<AddProductUseCaseResponse> execute(AddProductUseCaseRequest request) throws MessagingException, TemplateException, SQLException, IOException {
         ProductEntity productEntity = AddProductConverter.toProduct(request);
 //        var productResponse = AddProductConverter.toResponse(productEntity);
-        productList.add(productEntity);
+        productDbRepository.save(DbMapper.toDbEntity(productEntity));
         return Optional.of(new AddProductUseCaseResponse());
     }
     private void validateAddProduct(AddProductUseCaseRequest request){
